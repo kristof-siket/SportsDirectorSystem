a11225751dc27781025eb00d57419d650357c915
kristof-siket <kristofsiket@gmail.com>
2018-03-19 08:23:26 +0100

    Model class and migration for Runalyzer function ("data analysis")

--- resources/views/competitions/details.blade.php
+++ resources/views/competitions/details.blade.php
@@ -25,18 +25,21 @@
             <h4 class="list-group-item-heading">Promoter</h4>
             <p class="list-group-item-text">{!! $competition->promoter->first_name . ' ' . $competition->promoter->last_name !!}</p>
         </a>
-
         <a href="#" class="list-group-item">
             <h4 class="list-group-item-heading">Distances</h4>
             <ul class="list-group">
-                @foreach($competition->distances as $distance)
-                    <a href="{{ route('results.enter', ['comp_id' => $competition->comp_id, 'dist_id' => $distance->distance_id]) }}">
-                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                            {!! $distance->distance->distance_name !!}
-                            <span class="badge badge-primary badge-pill">{!! count($distance->competition->results->where('result_distance', $distance->distance_id)) !!}</span>
-                        </li>
-                    </a>
-                @endforeach
+                @if(empty($competition->distances))
+                    <b>At the moment, there are no distances for this competition!</b>
+                @else
+                    @foreach($competition->distances as $distance)
+                        <a href="{{ route('results.enter', ['comp_id' => $competition->comp_id, 'dist_id' => $distance->distance_id]) }}">
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                {!! $distance->distance->distance_name !!}
+                                <span class="badge badge-primary badge-pill">{!! count($distance->competition->results->where('result_distance', $distance->distance_id)) !!}</span>
+                            </li>
+                        </a>
+                    @endforeach
+                @endif
             </ul>
         </a>
     </div>
